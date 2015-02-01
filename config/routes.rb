d4063407e815f0d022bdfbc5c84a270d01689054
wadror <wadror@wadror-VirtualBox.(none)>
2015-02-01 22:30:07 +0200

    ex4

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,9 @@ Rails.application.routes.draw do
 
   resource :session, only: [:new, :create, :delete]
 
+  get 'signin', to: 'sessions#new'
+  delete 'signout', to: 'sessions#destroy'
+
   root 'breweries#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
